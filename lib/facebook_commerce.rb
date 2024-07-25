bfdecca56e5b68ead40b8a6f9b7c158499bbe6c1
Piers Chambers <piers@varyonic.com>
2024-07-25 13:51:59 -0400

    Initial implementation.

--- lib/facebook_commerce.rb
+++ lib/facebook_commerce.rb
@@ -2,7 +2,186 @@
 
 require_relative "facebook_commerce/version"
 
+require 'benchmark'
+require 'json'
+require 'logger'
+require 'securerandom'
+
 module FacebookCommerce
+  BASE_URL = 'https://graph.facebook.com/'
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.configure(&block)
+    yield @config
+  end
+
+  def config
+    @config
+  end
+
+  class Api
+    class UnexpectedHttpResponse < StandardError
+      def initialize(response)
+        super response.message || response.code
+      end
+    end
+    
+    attr_reader :access_token # See https://business.facebook.com/commerce_permission_wizard
+    attr_reader :cms_id
+    attr_accessor :logger
+
+    # config[:cms_id]
+    # config[:access_token]
+    def initialize(config = super.config)
+      @cms_id = config.fetch(:cms_id)
+      @access_token = config.fetch(:access_token)
+      @logger = config[:logger] || Logger.new(STDOUT)
+    end
+
+    protected
+
+    def get(path, data = {})
+      path = "#{path}?#{URI.encode_www_form(data.merge(access_token: access_token))}"
+      JSON.parse(send_request('GET', BASE_URL + path))
+    end
+
+    def post(path, data = {})
+      data.merge!(access_token: access_token)
+      JSON.parse(send_request('POST', BASE_URL + path, data))
+    end
+
+    # Accepts hash of fields to send.
+    # Returns response body if successful else raises exception.
+    def send_request(method, path, fields = {})
+      uri = URI(path)
+      connection = https(uri)
+      data = fields.to_a.map { |x| "#{x[0]}=#{x[1]}" }.join("&")
+      response = log_request_response(method, uri, data) do |m, u, d|
+        connection.send_request(m, u.to_s, d)
+      end
+      fail_unless_expected_response response, Net::HTTPSuccess
+      response.body
+    end
+
+    def https(uri)
+      Net::HTTP.new(uri.host, uri.port).tap do |http|
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      end
+    end
+  
+    # Log method, URI, data
+    # Start timer.
+    # Yield method, URI, data.
+    # Log response and time taken.
+    def log_request_response(method, uri, data = nil)
+      logger.info "[#{self.class.name}] request = #{method} #{uri}#{data ? '?' + data : ''}"
+      response = nil
+      tms = Benchmark.measure do
+        response = yield method, uri, data
+      end
+      logger.info("[#{self.class.name}] response (#{(tms.real*1000).round(3)}ms): #{response.inspect} #{response.body}")
+      response
+    end
+
+    def fail_unless_expected_response(response, *allowed_responses)
+      unless allowed_responses.any? { |allowed| response.is_a?(allowed) }
+        logger.error "#{response.inspect}: #{response.body}"
+        raise UnexpectedHttpResponse, response
+      end
+      response
+    end
+  end
+
+  # https://developers.facebook.com/docs/commerce-platform/order-management/order-api
+  class OrderApi < Api
+    # By default the list_orders method will return orders in the CREATED state.
+    # @option params updated_before [String] Unix timestamp
+    # @option params updated_after [String] Unix timestamp
+    # state The state of the order. The default is CREATED. FB_PROCESSING, IN_PROGRESS, COMPLETED
+    def list_orders(params = {})
+      get("#{cms_id}/commerce_orders", params).fetch('data')
+    end
+
+    # @param order_id [String]
+    # @option params fields [String] Comma-separated list of fields to include in the response
+    def get_order_details(order_id, params = {})
+      get(order_id, params)
+    end
+  end
+
+  # https://developers.facebook.com/docs/commerce-platform/order-management/acknowledgement-api
+  class AcknowledgementApi < Api
+    def associate_app
+      post("#{cms_id}/order_management_apps")
+    end
+
+    def acknowledge_order(order_id, merchant_order_reference = nil)
+      data = { idempotency_key: SecureRandom.uuid }
+      data[:merchant_order_reference] = merchant_order_reference if merchant_order_reference
+      post("#{order_id}/acknowledge_order", data)
+    end
+  end
+
+  # https://developers.facebook.com/docs/commerce-platform/order-management/fulfillment-api
+  class FulfillmentApi < Api
+    # @param order_id [String] Facebook order ID
+    # @param items [Array<Hash>] Array of item hashes (retailer_id|product_id, quantity)
+    # @param tracking_info [Hash] Tracking information (carrier, tracking_number, shipping_method_name)
+    def attach_shipment(order_id, items, tracking_info, external_shipment_id = nil)
+      data = { 
+        items: items.to_json,
+        tracking_info: tracking_info.to_json,
+        idempotency_key: SecureRandom.uuid
+      }
+      data[:external_shipment_id] = external_shipment_id if external_shipment_id
+
+      post("#{order_id}/shipments", data)
+    end
+  end
+
+  class CancellationRefundApi < Api
+    # @param order_id [String] Facebook order ID
+    # @return [Hash] Cancellation response, eg. { success: true}
+    def cancel_order(order_id)
+      data = { idempotency_key: SecureRandom.uuid }
+      post("#{order_id}/cancellations", data)
+    end
+
+    # @param order_id [String] Facebook order ID
+    # @param reason_code [String] Reason code for the refund, eg. 'REFUND_REASON_OTHER'
+    # @param items [Array<Hash>] Item hashes (retailer_id|product_id, quantity), required if partial refund
+    # @return [Hash] Refund response, eg. { success: true}
+    def refund_order(order_id, reason_code, items = nil)
+      data = { reason_code: reason_code, idempotency_key: SecureRandom.uuid }
+      post("#{order_id}/refunds", data)
+    end
+  end
+
+  class ReturnApi < Api
+    # @param order_id [String] Facebook order ID
+    # @param items [Array<Hash>] Array of item hashes (item_id|retailer_id, quantity, reason)
+    # @param return_status [String] Reason code for the return,
+    #   eg. 'REQUESTED', 'APPROVED', 'DISAPPROVED', 'REFUNDED', 'MERCHANT_MARKED_COMPLETED'
+    # @return [Hash] Return response, eg. { id: '1234567890' }
+    def create_return(order_id, items, return_status, return_message, merchant_return_id)
+      data = {
+        items: items.to_json,
+        return_status: return_status,
+        return_message: return_message,
+        merchant_return_id: merchant_return_id
+      }
+      post("#{order_id}/returns", data)
+    end
+
+    # @param return_id [String] Facebook return ID
+    # @param update_event [String] Reason code for the return, 'ACCEPT_RETURN' or 'CLOSE_RETURN'
+    # @option options [String] notes Notes for the return
+    # @option options [String] merchant_return_id Merchant return ID
+    # @option options [Array<Hash>] return_shipping_labels Array of shipping label hashes
+    #         (carrier, service_name, tracking_number, file_handle, cost)
+    def update_return(return_id, update_event, options = {})
+      post("#{return_id}/update_return", options.merge(update_event: update_event))
+    end
+  end
 end
--- spec/facebook_commerce_spec.rb
+++ spec/facebook_commerce_spec.rb
@@ -5,7 +5,141 @@ RSpec.describe FacebookCommerce do
     expect(FacebookCommerce::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  let(:config) { { cms_id: '123', access_token: 'abc' } }
+
+  describe described_class::OrderApi do
+    subject { described_class.new(config) }
+
+    describe '#list_orders' do
+      let(:params) { { state: 'CREATED' } }
+      let(:response) { { data: 'orders' }.to_json }
+      
+      before do
+        allow(subject).to receive(:send_request).and_return(response)
+      end
+      
+      it 'returns the orders' do
+        expect(subject.list_orders(params)).to eq('orders')
+      end
+    end
+
+    describe '#get_order_details' do
+      let(:order_id) { '123' }
+      let(:response) { { id: order_id }.to_json }
+      
+      before do
+        allow(subject).to receive(:send_request).and_return(response)
+      end
+
+      it 'returns the order' do
+        expect(subject.get_order_details(order_id)).to eq('id' => order_id)
+      end
+    end
+  end
+
+  describe described_class::AcknowledgementApi do
+    subject { described_class.new(config) }
+
+    describe '#associate_app' do
+      let(:response) { { success: true }.to_json }
+
+      before do
+        allow(subject).to receive(:send_request).and_return(response)
+      end
+
+      it 'returns the result' do
+        expect(subject.associate_app).to eq('success' => true)
+      end
+    end
+
+    describe '#acknowledge_order' do
+      let(:order_id) { '123' }
+      let(:response) { { state: 'IN_PROGRESS' }.to_json }
+      
+      before do
+        allow(subject).to receive(:send_request).and_return(response)
+      end
+
+      it 'returns the result' do
+        expect(subject.acknowledge_order(order_id)).to eq('state' => 'IN_PROGRESS')
+      end
+    end
+  end
+
+  describe described_class::FulfillmentApi do
+    subject { described_class.new(config) }
+
+    describe '#attach_shipment' do
+      let(:order_id) { '123' }
+      let(:response) { { success: true }.to_json }
+      
+      before do
+        allow(subject).to receive(:send_request).and_return(response)
+      end
+
+      it 'returns the result' do
+        expect(subject.attach_shipment(order_id, [], '')).to eq('success' => true)
+      end
+    end
+  end
+
+  describe described_class::CancellationRefundApi do
+    subject { described_class.new(config) }
+
+    describe '#cancel_order' do
+      let(:order_id) { '123' }
+      let(:response) { { success: true }.to_json }
+      
+      before do
+        allow(subject).to receive(:send_request).and_return(response)
+      end
+
+      it 'returns the result' do
+        expect(subject.cancel_order(order_id)).to eq('success' => true)
+      end
+    end
+
+    describe '#refund_order' do
+      let(:order_id) { '123' }
+      let(:response) { { success: true }.to_json }
+      
+      before do
+        allow(subject).to receive(:send_request).and_return(response)
+      end
+
+      it 'returns the result' do
+        expect(subject.refund_order(order_id, 'REFUND_REASON_OTHER')).to eq('success' => true)
+      end
+    end
+  end
+
+  describe described_class::ReturnApi do
+    subject { described_class.new(config) }
+
+    describe '#create_return' do
+      let(:order_id) { '123' }
+      let(:response) { { id: '1234567890' }.to_json }
+      
+      before do
+        allow(subject).to receive(:send_request).and_return(response)
+      end
+
+      it 'returns the result' do
+        expect(subject.create_return(order_id, [], 'RETURN_REASON_OTHER', '', '')).to eq('id' => '1234567890')
+      end
+    end
+
+    describe '#update_return' do
+      let(:return_id) { '123' }
+      let(:response) { { success: true }.to_json }
+      
+      before do
+        allow(subject).to receive(:send_request).and_return(response)
+      end
+
+      it 'returns the result' do
+        expect(subject.update_return(return_id, 'ACCEPT_RETURN')).to eq('success' => true)
+      end
+    end
   end
 end
